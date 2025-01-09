c730bacd77d2cff35d14f590c0b68f1283dd0954
Ivan <ivan03ds@gmail.com>
2025-01-09 12:54:23 +0100

    formazione fino a cap 6

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,13 @@
 export default function Page() {
-  return <p>Dashboard page!</p>
+  return(
+    <div>
+      <p>Dashboard page!</p>
+      <p>
+        Go to <a href="http://localhost:3000/dashboard/customers">customer page</a>
+      </p>
+      <p>
+        Go to <a href="http://localhost:3000/dashboard/invoices">invoices page</a>
+      </p>
+    </div>
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -44,6 +44,9 @@ export default function Page() {
             alt='Screenshots of the dashboard project showing mobile version' />
         </div>
       </div>
+      <p>
+        Go to the <a href="http://localhost:3000/dashboard">dashboard</a>
+      </p>
     </main>
   );
 }
