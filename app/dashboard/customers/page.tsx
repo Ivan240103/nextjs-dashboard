0c49d75fa269e2982a5c7a96a6600a5eed7e89ba
Ivan <ivan03ds@gmail.com>
2025-01-21 10:40:20 +0100

    customer page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,12 +1,22 @@
 import CustomersTable from '@/app/ui/customers/table';
+import { FormattedCustomersTable } from '@/app/lib/definitions';
+import { fetchFilteredCustomers } from '@/app/lib/data';
 import { Metadata } from 'next';
  
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+  }>
+}) {
+  const searchParams = await props.searchParams
+  const query = searchParams?.query || ''
+  const customers: FormattedCustomersTable[] = await fetchFilteredCustomers(query)
+
   return(
-    <p>Customers page</p>
+    <CustomersTable customers={customers} />
   )
 }
